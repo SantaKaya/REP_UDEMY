aca0b17e9627cb5f49fde9e3068da604115c1062
SantaKaya <gabrielle.tripodi@gmail.com>
2020-10-07 20:28:40 -0300

    ADD (p4.c) ALT_NAME(pastas)

---
+++ PROG_C/S5(estruturas_repeticao)/p4.c
@@ -0,0 +1,31 @@
+// Estruturas de repetição, for, while, do..while.
+
+/* Utilizado o FOR
+    Faça um progama no qual receba e some 5 numeros inteiros e apresente a soma no final.
+*/
+                        
+#include <stdio.h>
+
+int main(){
+
+    //declaração_variaveis
+    int numero, soma = 0; //Inicializa soma para não receber lixo
+
+    //inicio_for 
+    for (int i = 0; i < 5; i++){//para o int i iniciando em 0; enquanto i < 5; incremente i em 1.
+
+    //entrada_dados
+    printf("Informe um numero:\n");
+    scanf("%d",&numero);
+
+    //processamento_dados
+    soma = soma + numero;
+    }//fim_for
+
+    //saida_dados
+    printf("A soma eh: %d",soma);
+    
+
+    return 0;
+}
+
