c1cf500d79fc0d9372a2d108240b31ca6d01a3d4
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-08 17:29:29 -0300

    ADD e ALT
    add ajuda.c e ajuda.h
    alt p20.c

---
+++ PROG_C/S9(funcoes_c)/4-ARQUIVOS_CABEÇALHOS/ajuda.h
@@ -0,0 +1,7 @@
+//biblioteca com prototipos de funções
+#include <stdio.h>
+#include <stdlib.h>
+
+void mensagem();
+int soma(int num1, int num2);
+int mult(int num1, int num2);
---
+++ PROG_C/S9(funcoes_c)/4-ARQUIVOS_CABEÇALHOS/p20.c
@@ -0,0 +1,40 @@
+/*
+    Vamos criar um progama que receba 2 numeros, execute a função de soma e multiplicação e passe esses valores para as
+    variaveis ret_s e ret_m.
+*/
+
+#include <stdio.h>
+#include <stdio.h>
+#include "ajuda.h" //biblioteca criada
+
+
+
+
+int main(){
+    printf("\n");
+
+    mensagem();
+
+    //d_v//
+    int n1, n2, ret_s, ret_m;
+
+    //e_d//
+    printf("Informe o primeiro numero:\n");
+    scanf("%d",&n1);
+    printf("Informe o segundo numero:\n");
+    scanf("%d",&n2);
+
+
+    //p_d//
+    ret_s = soma(n1,n2);
+    printf("A soma de %d com %d eh %d", n1, n2, ret_s);
+
+    ret_m = mult(n1,n2);
+    printf("A multiplicacao de %d com %d eh %d", n1, n2, ret_m);
+
+    //s_d//
+
+
+    printf("\n");
+    return 0;
+}
