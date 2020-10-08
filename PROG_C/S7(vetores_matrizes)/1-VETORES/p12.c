b137bcc56c870d22a2b95588c59c7e8a9f6ba37a
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-08 12:48:41 -0300

    FIM(p12.c) | ADD(p13.c)

---
+++ PROG_C/S7(vetores_matrizes)/1-VETORES/p12.c
@@ -0,0 +1,53 @@
+/*
+
+*/
+
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+
+    //declarando_variaveis
+    
+    //vetore e string
+    char nome[50];
+    printf("Qual seu nome?\n");
+    gets(nome);
+    printf("Olah %s\n",nome);
+
+
+
+
+    //vetores e caracteres
+    char letras[26];//0..25
+    //'b'
+    int contador = 0;
+    for (int i = 97 ; i <= 122 ; i++){
+        letras[contador] = i;
+        contador = contador + 1;
+    }
+    //imprimindo as letras e seus valores em decimal.
+    for (int i = 0 ; i < 26; i++){
+        printf("%d == %c\n",letras[i],letras[i]);
+    }
+
+
+    //vetores de inteiros
+    int numeros[10];
+
+    //vetores e reais
+    float valores[5];
+
+
+
+
+    //entrada_dados
+
+    //processamento_dados
+
+    //saida_dados
+
+
+
+    return 0;
+}
