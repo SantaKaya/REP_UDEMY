110029a60fc13fbf74bb9174a92912ad17cdeaae
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-08 17:45:04 -0300

    DEL p20.exe ALT ajuda.c

--- PROG_C/S9(funcoes_c)/4-ARQUIVOS_CABEÇALHOS/ajuda.c
+++ PROG_C/S9(funcoes_c)/4-ARQUIVOS_CABEÇALHOS/ajuda.c
@@ -6,14 +6,11 @@
 void mensagem(){
     printf("Bem vindo...\n");
 }
-
 /////////////////////OPERAÇÕES/////////////////////////////////////////////////
-
 //soma//
 int soma(int num1, int num2){
     return num1 + num2;
 }
-
 //multiplicação//
 int mult(int num1, int num2){
     return num1 * num2;
