49f56d66d6d6c71f2ac927500a050b53716ba9fc
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-08 18:34:45 -0300

    Update p21.c

---
+++ PROG_C/S10(ponteiros)/1-SOBRE_PONTEIROS/p21.c
@@ -0,0 +1,22 @@
+/*
+
+*/
+
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+    printf("\n");
+
+    //declarando_variaveis
+
+    //entrada_dados
+
+    //processamento_dados
+
+    //saida_dados
+
+
+    printf("\n");
+    return 0;
+}
