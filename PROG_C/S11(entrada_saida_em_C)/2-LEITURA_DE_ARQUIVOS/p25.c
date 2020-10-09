dc3542bb42d56aa67128c22b77908d3cf6d37134
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-09 18:12:30 -0300

    ADD
    arquivo.txt
    p25.c
    p26.c
    p27.c

---
+++ PROG_C/S11(entrada_saida_em_C)/2-LEITURA_DE_ARQUIVOS/p25.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    FILE *arq;
+    char c;
+
+    arq = fopen("Xarquivo.txt","r");
+    
+    if(arq){//sempre importante fazer essa verificação.
+        while((c = getc(arq)) != EOF){// EOF = AND OF FILE
+            printf("%c",c);
+        }
+    }else{
+        printf("Nao achei o arquivo!");
+    }
+
+
+    fclose(arq);
+
+    return 0;
+}
---
+++ PROG_C/S11(entrada_saida_em_C)/2-LEITURA_DE_ARQUIVOS/p26.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(){
+
+    FILE *arq;
+    char nome[10], *resultado;
+
+    arq = fopen("arquivo.txt","r");
+
+    if(arq){
+        while(!feof(arq)){//feof - file and of file , ira fazer o loop enquanto n chegar ao fim de arquivo
+            resultado = fgets(nome, 10, arq);//fgets - file gets
+            printf("Resultado: %d\n", resultado);
+            if(resultado){
+                printf("%s",nome);
+            }
+
+        } 
+    }else{
+        printf("Nao achei o arquivo.");
+    }
+
+
+    fclose(arq);
+    return 0;
+}
