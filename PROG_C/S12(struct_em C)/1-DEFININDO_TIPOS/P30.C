5b8f3d9c26c5042fbb69ec9442e347a4075008ce
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-10 09:43:54 -0300

    Create P30.C

---
+++ PROG_C/S12(struct_em C)/1-DEFININDO_TIPOS/P30.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+struct st_aluno{
+    char matricula[10]; 
+    char nome[100];
+    char curso[50];
+    int ano_nascimento;
+};
+
+
+int main(){
+
+    struct st_aluno aluno1;
+
+    printf("Informe a matricula do aluno:\n");
+    fgets(aluno1.matricula,10,stdin);
+
+    printf("Informe o nome do aluno:\n");
+    fgets(aluno1.nome,100,stdin);
+
+    printf("Informe o curso do aluno:\n");
+    fgets(aluno1.curso,50,stdin);
+
+    printf("Informe o ano de nascimento do aluno:\n");
+    scanf("%d",&aluno1.ano_nascimento);
+
+    printf("===========================Dados do aluno=====================\n");
+    printf("Matricula :%s\n", aluno1.matricula);
+    printf("Nome do Aluno:%s\n", aluno1.nome);
+    printf("Curso :%s\n", aluno1.curso);
+    printf("Ano de Nascimento:%d\n", aluno1.ano_nascimento);
+    
+    
+    return 0;
+}
