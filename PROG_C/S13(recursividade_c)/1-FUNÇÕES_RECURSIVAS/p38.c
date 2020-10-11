0731db7c94bda3dd1ad09961055d2bcb75b0b901
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-10 21:08:58 -0300

    FIM RECURSIVIDADE

---
+++ PROG_C/S13(recursividade_c)/1-FUNÇÕES_RECURSIVAS/p38.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+//Recursividade - É o ato de uma função chamar a si mesma.
+/*
+*   5 primeiros numeros da sequencia fibonacci.
+*   1 1 2 3 5 
+*/
+
+
+int contador = 1;
+
+int fib(int n){
+
+    if(n==0){
+         return 0;
+    }
+
+    if(n==1){
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+
+
+}//fim_fib
+
+int main(){
+
+    int qtd;
+    
+    printf("Informe o tamanho da sequencia fibonacci:\n");
+    scanf("%d",&qtd);
+    printf("\n");
+    for(int i = 0; i < qtd; i++){
+        printf("%d\n", fib(i + 1));
+    }
+
+    return 0;
+}
