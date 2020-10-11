0ad81ea71bc4e455d745ec23395f445dd60924b1
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-10 21:26:50 -0300

    add
    pdf
    p39

---
+++ PROG_C/S14(diretivas_compilação)/1-IFNDEF/P39.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// constantes
+
+#ifndef PI
+    #define PI 3.14567 //constante
+#endif
+
+
+int main(){
+
+    int valor =5; //variavel
+
+    valor = 467;
+
+    printf("O valor eh %d\n", valor);
+
+    printf("2 x PI vale %f\n", (2 * PI));
+
+
+    return 0;
+}
