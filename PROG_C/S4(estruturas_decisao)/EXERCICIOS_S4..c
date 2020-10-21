a39ff4042e90985a8ff8eefde3f879af634d412c
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-21 19:07:25 -0300

    FIM E03

--- PROG_C/S4(estruturas_decisao)/EXERCICIOS_S4..c
+++ PROG_C/S4(estruturas_decisao)/EXERCICIOS_S4..c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
-//MAIN E01
+
+//////////////////////////QUESTOES/////////////////////////////////////////
+
+/*MAIN E03
+int main(){
+    printf("\n*******************************************************\n");
+
+    float n1;
+
+    printf("Digite um numero real:\n");
+    scanf("%f",&n1);
+
+    if( n1 / 2 >= 0){
+        printf("Raiz Quadrada = %.2f",sqrt(n1));
+    }else{
+        printf("Numero %.2f ao Quadrado = %.2f", n1, n1*n1);
+    }
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+
+
+/*MAIN E02
+int main(){
+    printf("\n*******************************************************\n");
+
+    int n1;
+
+    printf("Digite um numero:\n");
+    scanf("%d",&n1);
+
+    if( n1 / 2  >= 0){
+        printf("%.2f",sqrt(n1));
+    }else{
+        printf("\nNumero invalido.\n");
+    }
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/*MAIN E01
 int main(){
     printf("\n*******************************************************\n");
 
@@ -16,13 +61,7 @@ int main(){
     }else{
         printf("Numero %d maior.",n2);
     }
-
-
-
-
-
-
-
     printf("\n*******************************************************\n");
     return 0;
 }
+*/
