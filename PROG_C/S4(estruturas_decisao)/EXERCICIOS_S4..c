4b03b51c56a845555a8b99a897ce61ba592b5720
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-10-30 12:58:09 -0300

    FIM E19

--- PROG_C/S4(estruturas_decisao)/EXERCICIOS_S4..c
+++ PROG_C/S4(estruturas_decisao)/EXERCICIOS_S4..c
@@ -1,10 +1,375 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 
 
 //////////////////////////QUESTOES/////////////////////////////////////////
+
+/*MAIN E19
+
+int main(){
+    printf("\n*******************************************************\n");
+
+    int num;
+
+    printf("Digite um numero:\n");
+    scanf("%d",&num);
+
+    if(num < 99){
+        int a = num / 10;//valor1 
+        int b = num % 10;//valor 2
+        int soma = a + b;
+        //printf("soma = %d\n",soma);
+        int div_3 = soma % 3;
+        //printf("div_3 = %d\n",div_3);
+
+        if(b == 0 || b == 5){
+            if(div_3 == 0){
+                printf("\nNumero divisivel por 3 e 5.");
+            }else{
+                printf("\nNumero divisivel por 5!\n");
+            }
+        }else if(div_3 == 0){
+            printf("\nNumero divisivel por 3!\n");
+        }else{
+            printf("\nNao divide nem por 3 nem por 5");
+        }
+    }
+
+    if(num >= 100 && num < 999){
+        int a = num / 100;//valor1 
+        int b = num % 100;//valor 2 e 3
+        int c = b / 10; // valor 2
+        int d = b % 10; // valor 3
+        int soma = a + c + d;
+        //printf("soma = %d\n",soma);
+        int div_3 = soma % 3;
+        //printf("div_3 = %d\n",div_3);
+
+        if(d == 0 || d == 5){
+            if(div_3 == 0){
+                printf("\nNumero divisivel por 3 e 5.");
+            }else{
+                printf("\nNumero divisivel por 5!\n");
+            }
+        }else if(div_3 == 0){
+            printf("\nNumero divisivel por 3!\n");
+        }else{
+            printf("\nNao divide nem por 3 nem por 5");
+        }
+    }
+
+    if(num >= 1000 && num < 9999){
+        int a = num / 1000;//valor1 
+        int b = num % 1000;//valor 2 3 4
+        int c = b / 100; // valor 2
+        int d = b % 100; // valor 3 4
+        int e = d / 10;//valor 3
+        int f = d % 10; //valor 4
+        int soma = a + c + e + f;
+        //printf("soma = %d",soma);
+        int div_3 = soma % 3;
+        //printf("div_3 = %d\n",div_3);
+        if(f == 0 || f == 5){
+            if(div_3 == 0){
+                printf("\nNUmero divisivel por 3 e 5.");
+            }else{
+                printf("\nNUmero divisivel por 5!\n");
+            }
+        }else if(div_3 == 0){
+            printf("\nNumero divisivel por 3!\n");
+        }else{
+            printf("\nNao divide nem por 3 nem por 5");
+        }
+    }
+
+    
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/*MAIN E18
+int main(){
+    printf("\n*******************************************************\n");
+
+    int num, n1, n2;
+
+    printf("1 - SOMA:\n");
+    printf("2 - subtração\n");
+    printf("3 - multiplicação\n");
+    printf("4 - divisao\n");
+
+    scanf("%d",&num);
+
+    switch(num){
+
+        case 1:
+            printf("Digite 2 valores:\n");
+            scanf("%d %d", &n1, &n2);
+
+            int soma = (n1 + n2);
+            printf("%d + %d = %d", n1, n2, soma);
+            break;
+
+        case 2 :
+            printf("Digite 2 valores:\n");
+            scanf("%d %d", &n1, &n2);
+
+            int sub = (n1 - n2);
+
+            printf("%d - %d = %d", n1, n2, sub);
+            break;
+        case 3 :
+            printf("Digite 2 valores:\n");
+            scanf("%d %d", &n1, &n2);
+
+            int mult = (n1 * n2);
+
+            printf("%d * %d = %d", n1, n2, mult);
+            break;
+        case 4 :
+            printf("Digite 2 valores:\n");
+            scanf("%d %d", &n1, &n2);
+
+            float div = ((float)n1 / (float)n2);
+            
+            printf("%d / %d = %.2f\n", n1, n2, div);
+            break;
+    }
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/* MAIN E17
+
+int main(){
+    printf("\n*******************************************************\n");
+
+    float b_M, b_m, alt;
+
+    printf("Digite a Base Maior, Base Menor e a Altura:\n");
+    scanf("%f %f %f", &b_M, &b_m, &alt);
+
+    while(b_M > 0 && b_m > 0){
+        float area = ((b_M + b_m)*alt)/2;
+        printf("Area do trapezio = %.2f", area);
+        break;
+    }
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/*MAIN E16
+
+int main(){
+    printf("\n*******************************************************\n");
+
+    int num;
+
+    printf("Digite um numero:\n");
+    scanf("%d",&num);
+    
+    switch(num){
+        case 1:
+            printf("Janeiro!\n");
+            break;
+        case 2:
+            printf("Fevereiro!\n");
+            break;
+        case 3:
+            printf("Marco!\n");
+            break;
+        case 4:
+            printf("Abril!\n");
+            break;
+        case 5:
+            printf("Maio!\n ");
+            break;
+        case 6:
+            printf("Junho!\n");
+            break;
+        case 7:
+            printf("Julho!\n");
+            break;
+        case 8:
+            printf("Agosto!\n");
+            break;
+        case 9:
+            printf("Setembro!\n");
+            break;
+        case 10:
+            printf("Outubro!\n");
+            break;
+        case 11:
+            printf("Novembro!\n");
+            break;
+        case 12:
+            printf("Dezembro!\n");
+            break;
+    }
+
+    printf("\n*******************************************************\n");
+
+    return 0;
+}
+*/
+
+/*MAIN E15
+int main(){
+    printf("\n*******************************************************\n");
+
+    int num;
+
+    printf("Digite um numero inteiro:\n");
+    scanf("%d",&num);
+
+    switch (num)
+    {
+    case 1:
+        printf("Domingo!\n");
+        break;
+    case 2:
+        printf("Segunda!");
+        break;
+    case 3:
+        printf("Terca!\n");
+        break;
+    case 4:
+        printf("Quarta!");
+        break;
+    case 5:
+        printf("Quinta!\n");
+        break;
+    case 6:
+        printf("Sexta!\n");
+        break;
+    case 7:
+        printf("Sabado!\n");
+        break;
+    }
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/*MAIN E14
+int main(){
+    printf("\n*******************************************************\n");
+
+    float n1, n2, n3;
+
+    printf("Digite as notas:\n");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    float med_p = (((n1* 2) + (n2 * 3) + (n3 * 5)) / 10);
+
+    if(med_p < 2.9){
+        printf("Aluno reprovado\nMedia = %.2f", med_p);
+    }else if(med_p <= 4.99){
+        printf("Aluno na Recuperacao!\nMedia = %.2f",med_p);
+    }else if(med_p > 4.9){
+        printf("Aluno aprovado!\nMedia = %.2f", med_p);
+    }
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/*MAIN E13
+int main(){
+    printf("\n*******************************************************\n");
+
+    float n1,n2,n3;
+
+    printf("Digite as notas:\n");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    float med_p = ((n1 * 1) + (n2 * 1) + (n3 * 2))/(1+1+2);
+
+    if(med_p < 60){
+        printf("Media = %.2f\nREPROVADO",med_p);
+    }else{
+        printf("Media = %.2f\n Aprovado",med_p);
+    }
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/*MAIN E12
+int main(){
+    printf("\n*******************************************************\n");
+
+    int num;
+
+    printf("Digite um numero:\n");
+    scanf("%d",&num);
+
+    double x = log(num);
+
+    if( num < 0){
+        printf("Numero invalido!\n");
+    }else{
+        printf("Logaritmo natural de %d = %lf", num, x);
+    }
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+*/
+
+/*MAIN E11
+
+int main(){
+    printf("\n*******************************************************\n");
+
+    int num, soma;
+
+    printf("Digite um numero inteiro:\n");
+    scanf("%d",&num);
+
+
+    if(num > 0){
+
+        if( num <= 99){
+            soma = (num / 10) + (num % 10);
+            printf("Soma = %d", soma);
+        }
+
+        if( num >= 100 && num <= 999){
+
+            soma = (num / 100) + ((num%100)/10) + ((num % 100) % 10);
+            printf("Soma = %d", soma);
+
+        }
+
+        if(num > 999){
+            soma = (num / 1000) + ((num%1000)/100) + (((num % 1000) % 100) / 10) + (((num % 1000) % 100) % 10);
+
+            printf("Soma = %d", soma);
+            
+
+        }
+
+    }else{
+        printf("Numero invalido.");
+    }
+
+    printf("\n*******************************************************\n");
+    return 0;
+}
+
+*/
 
 /*MAIN E10
 int main(){
