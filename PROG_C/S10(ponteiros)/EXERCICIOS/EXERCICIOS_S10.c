44503b0f8c9e32bc9aa4726b5c037cd4b2224c53
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2020-11-30 09:53:45 -0300

    INI S10, FIM E01

---
+++ PROG_C/S10(ponteiros)/EXERCICIOS/EXERCICIOS_S10.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+//////////////////////////////////////QUESTOES//////////////////////////////////////////
+
+/*MAIN 01
+int main(){
+    int a,*pa;
+    char b, *pb;
+    float c, *pc;
+
+    a = 1;
+    b = 'b';
+    c = 3.4;
+
+    printf("\n==========ANTES=============\n");
+
+    printf("A = |%d|\nB = |%c|\nC = |%.2f|\n",a,b,c);
+    printf("&A = |%d|\n&B = |%d|\n&C = |%d|\n",&a,&b,&c);
+    
+    printf("PA = |%d|\n PB = |%d|\nPC = |%d|\n",pa,pb,pc);
+    printf("&PA = |%d|\n &PB = |%d|\n&PC = |%d|\n",&pa,&pb,&pc);
+
+    pa = &a;
+    pb = &b;
+    pc = &c;
+
+    *pa = 2;
+    *pb = 'c';
+    *pc = 3.77;
+
+    printf("\n==========DEPOIS=============\n");
+
+    printf("A = |%d|\nB = |%c|\nC = |%.2f|\n",a,b,c);
+    printf("&A = |%d|\n&B = |%d|\n&C = |%d|\n",&a,&b,&c);
+
+    printf("PA = |%d|\nPB = |%d|\nPC = |%d|\n",pa,pb,pc);
+    printf("&PA = |%d|\n &PB = |%d|\n&PC = |%d|\n",&pa,&pb,&pc);
+
+    return 0;
+}
+*/
