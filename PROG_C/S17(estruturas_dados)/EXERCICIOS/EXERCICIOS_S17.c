ab7a1bcf5c68f193f6c4c547259430e55a91a85d
SantaKaya <gabrielle.tripodi@gmail.com>
2020-12-08 13:58:31 -0300

    INI S17 FIm 01 02

--- PROG_C/S17(estruturas_dados)/EXERCICIOS/EXERCICIOS_S17.c
+++ PROG_C/S17(estruturas_dados)/EXERCICIOS/EXERCICIOS_S17.c
@@ -6,7 +6,21 @@
 
 /////// QUESTOES ///////////////
 
+//MAIN E02
+/**
+ * O espaço de memoria que será utilizado para todos os elementos do conjunto
+ * é dimensionado previamente.
+ * Esse espaço dpermanecerá reservado durante toda a execução do progama, mesmo que 
+ * o mesmo não esteja sendo efetivamente utilizado por completo.
+ * 
+ */
+
+
 
 //MAIN E01
+/**
+ * Os elementos alocados ficam necessariamente em posições de memoria sequenciais(adjacentes)
+ * 
+ */
 
 
