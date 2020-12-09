db243ce4a0f90455594cd886c5693887f7d50170
SantaKaya <gabrielle.tripodi@gmail.com>
2020-12-09 10:19:04 -0300

    INI s17P2 FIM e01

--- PROG_C/S17(estruturas_dados)/EXERCICIOS/BBTK_S17.h
+++ PROG_C/S17(estruturas_dados)/EXERCICIOS/BBTK_S17.h
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.H>
 #include <math.h>
--- PROG_C/S17(estruturas_dados)/EXERCICIOS/EXERCICIOS_S17.c
+++ PROG_C/S17(estruturas_dados)/EXERCICIOS/EXERCICIOS_S17.c
@@ -6,13 +6,18 @@
 
 /////// QUESTOES ///////////////
 
-//MAIN E04
-int main(){
-
-
-
-    return 0;
-}
+//MAIN 03
+/**
+ * VANTAGENS:
+ *  - A inserção e remoção de elementos podem ser feitas sem deslocar os itens seguintes da lista.
+ *  - Não ha necessidade de previsão do numero de elementos da lista; o espaço necessario é alocado em tempo de execução;
+ *  - Facilita o gerenciamento de varias listas (fusão, divisão).
+ * 
+ * DESVANTAGENS:
+ *  - Acesso indireto aos elementos;
+ *  - Tempo variavel para acessar os elementos (depende da posição do elemento);
+ *  - Gasto de memoria maior pela necessidade de um novo campo para o ponteiro.
+ */
 
 
 
